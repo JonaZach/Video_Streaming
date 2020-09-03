20c19166527379d23d48f1bb81a93070ea535824
Jonathan Zach <jonatanz@gmail.com>
2020-09-03 18:01:36 +0300

    Updates

---
+++ precache-manifest.c696485d41e4a243c95d6a1bcf12731c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "864ea2cc5a265d461ebbaf4b9a7b5803",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c467ba83d6fdc225312",
+    "url": "/static/css/main.e1baa10e.chunk.css"
+  },
+  {
+    "revision": "b18335d551dc639ef2bc",
+    "url": "/static/js/2.3d1f69fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d1f69fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c467ba83d6fdc225312",
+    "url": "/static/js/main.ed5cb319.chunk.js"
+  },
+  {
+    "revision": "2b94ecada3f6cad98a2b",
+    "url": "/static/js/runtime-main.f1e90bb9.js"
+  }
+]);
